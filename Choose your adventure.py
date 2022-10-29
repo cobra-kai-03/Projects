a4ab625710e60ee4991707a32ad0ebd0e3b40d7c
cobra-kai-03 <78267022+cobra-kai-03@users.noreply.github.com>
2022-10-29 16:41:22 +0530

    Create Choose your adventure.py

---
+++ Choose your adventure.py
@@ -0,0 +1,10 @@
+name=input("Type your name: ")
+print("Welcome",name,"to this adventur")
+
+ans=input("You are on a dirt road left or right ").lower()
+if ans=="left":
+    print("Hi")
+elif ans=="right":
+    print("Hello")
+else:
+    print("Invalid answer")
